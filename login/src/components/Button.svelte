a0ca80c030bc776fe7b754a0656c9bd739dabf64
Riley Wilton <meetingprince34@outlook.com>
2021-02-03 15:18:05 -0500

    Nice login form

---
+++ login/src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script>
+    import './button.scss'
+    export let content;
+    export let type = "button";
+    export let form;
+</script>
+
+<button class="pwrx-button" form="{form}" type="{type}">{ content }</button>
