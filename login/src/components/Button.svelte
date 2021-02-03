26582474a541c82c41f7660d9e89b82ebf23f6ce
Riley Wilton <meetingprince34@outlook.com>
2021-02-03 15:28:09 -0500

    Using svelte for such a small form was not a good idea.

--- login/src/components/Button.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    import './button.scss'
-    export let content;
-    export let type = "button";
-    export let form;
-</script>
-
-<button class="pwrx-button" form="{form}" type="{type}">{ content }</button>
--- main.py
+++ main.py
@@ -13,6 +13,8 @@ app = FastAPI(
     }
 )
 
+app.mount("/static",StaticFiles(directory="static"),name="static")
+
 templates = Jinja2Templates(directory="templates")
 
 
