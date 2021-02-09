110ece3c11c117fdb813f9f85a3fa6aa581bffba
Riley Wilton <meetingprince34@outlook.com>
2021-02-09 11:11:27 -0500

    Working on installer

--- install.py
+++ install.py
@@ -13,33 +13,52 @@ import pymongo
 console = Console()
 
 
+def request_mongodb():
+    console.print("1. Connection String")
+    console.print("2. Enter host and port manually")
 
+    connection_method = Prompt.ask("Which of the above methods to you want to use to connect to mongodb?", choices=["1","2"])
 
-console.print("1. Connection String")
-console.print("2. Enter host and port manually")
-
-connection_method = Prompt.ask("Which of the above methods to you want to use to connect to mongodb?", choices=["1","2"])
-
-if connection_method == "1":
-    connection_string = Prompt.ask("Please enter your mongodb connection string")
-    
-
-elif connection_method == "2":
-    host = Prompt.ask("Please enter your mongodb host")
-    port = Prompt.ask("Please enter your mongodb port", default="27017")
-    while not port.isdecimal():
-        print("[red]Please enter a valid port number[/red]")
-        port = Prompt.ask("Please enter your mongodb port", default="27017")
-    with console.status("Trying to connect to mongodb"):
-        server_info = None
-        mongo_client = None
-        try:
-            mongo_client = pymongo.MongoClient(host,int(port))
-            server_info = mongo_client.server_info()
-        except:
+    if connection_method == "1":
+        connection_string = Prompt.ask("Please enter your mongodb connection string")
+        with console.status("Trying to connect to mongodb"):
+            server_info = None
+            mongo_client = None
+            try:
+                mongo_client = pymongo.MongoClient(connection_string)
+                server_info = mongo_client.server_info()
+            except:
+                console.print("[red]Unable to connect to mongodb[/red]")
+        if server_info:
+            console.print("[green]Successfully connected to mongodb[/green]")
+            return mongo_client
+        else:
             console.print("[red]Unable to connect to mongodb[/red]")
-    if server_info:
-        console.print("[green]Successfully connected to mongodb[/green]")
-    else:
-        console.print("[red]Unable to connect to mongodb[/red]")
+            request_mongodb()
+
+    elif connection_method == "2":
+        host = Prompt.ask("Please enter your mongodb host", default="localhost")
+        port = Prompt.ask("Please enter your mongodb port", default="27017")
+        while not port.isdecimal():
+            print("[red]Please enter a valid port number[/red]")
+            port = Prompt.ask("Please enter your mongodb port", default="27017")
+        with console.status("Trying to connect to mongodb"):
+            server_info = None
+            mongo_client = None
+            try:
+                mongo_client = pymongo.MongoClient(host,int(port))
+                server_info = mongo_client.server_info()
+            except:
+                console.print("[red]Unable to connect to mongodb[/red]")
+        if server_info:
+            console.print("[green]Successfully connected to mongodb[/green]")
+            return mongo_client
+        else:
+            console.print("[red]Unable to connect to mongodb[/red]")
+            request_mongodb()
+
+# Request the mongodb connection
+mongo = request_mongodb()
+
+
 database_name = Prompt.ask("Please enter the name for the database that schuaro will use", default="schuaro")
