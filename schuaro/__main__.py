0f8df7a62d454c2cc07f7a73c161bd918aa798ff
Riley Wilton <meetingprince34@outlook.com>
2021-05-12 16:52:20 -0400

    Reset. Again

--- schuaro/__main__.py
+++
@@ -1,6 +0,0 @@
-import uvicorn
-from schuaro import app
-import os
-
-if __name__ == "__main__":
-    uvicorn.run("schuaro:app", host="0.0.0.0", port=8000, log_level="info", reload=os.environ.get('schuaro_dev')=='True')
