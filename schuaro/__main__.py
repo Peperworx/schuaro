a2cee298369445bdd364100813aeb76dcc9c266d
Riley Wilton <meetingprince34@outlook.com>
2021-05-12 23:31:39 -0400

    Still working on http 3

--- schuaro/__main__.py
+++ schuaro/__main__.py
@@ -3,7 +3,13 @@ from hypercorn.config import Config
 from hypercorn.asyncio import serve
 
 from schuaro import app
+import os
 
 c = Config()
-c.quic_bind = ["localhost:4433"]
-asyncio.run(serve(app, Config()))
+c.certfile = os.path.join(os.getcwd(),"certificate.pem")
+c.keyfile = os.path.join(os.getcwd(),"key.pem")
+c.quic_bind = ["127.0.0.1:4433"]
+c.bind = ["127.0.0.1:443"]
+c.insecure_bind = ["127.0.0.1:80"]
+
+asyncio.run(serve(app, c))
