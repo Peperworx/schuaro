fbc9c7a12ef6f5abf88200bfb7b4577a69cc1afd
Riley Wilton <meetingprince34@outlook.com>
2021-05-12 23:34:24 -0400

    Use new address

--- schuaro/__main__.py
+++ schuaro/__main__.py
@@ -8,8 +8,8 @@ import os
 c = Config()
 c.certfile = os.path.join(os.getcwd(),"certificate.pem")
 c.keyfile = os.path.join(os.getcwd(),"key.pem")
-c.quic_bind = ["127.0.0.1:4433"]
-c.bind = ["127.0.0.1:443"]
-c.insecure_bind = ["127.0.0.1:80"]
+c.quic_bind = ["0.0.0.0:4433"]
+c.bind = ["0.0.0.0:443"]
+c.insecure_bind = ["0.0.0.0:80"]
 
 asyncio.run(serve(app, c))
