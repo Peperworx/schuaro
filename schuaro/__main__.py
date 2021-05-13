ba241e2fd6659a3032a249ec750a8bad665ac976
Riley Wilton <meetingprince34@outlook.com>
2021-05-13 00:37:26 -0400

    IT WORKS

--- schuaro/__main__.py
+++ schuaro/__main__.py
@@ -1,6 +1,6 @@
-import asyncio
+import trio
 from hypercorn.config import Config
-from hypercorn.asyncio import serve
+from hypercorn.trio import serve
 
 from schuaro import app
 import os
@@ -11,5 +11,4 @@ c.keyfile = os.path.join(os.getcwd(),"key.pem")
 c.quic_bind = ["0.0.0.0:4433"]
 c.bind = ["0.0.0.0:443"]
 c.insecure_bind = ["0.0.0.0:80"]
-
-asyncio.run(serve(app, c))
+trio.run(serve, app, c)
